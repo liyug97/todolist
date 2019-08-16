10598595e55136ad5e7722e4f0de6b6f43fb1fde
970705 <47617296+970705@users.noreply.github.com>
2019-08-16 13:38:34 +0800

    todolist

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <li @click="handleDelete">{{ content }}</li>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["content", "index"],
+    methods: {
+      handleDelete: function() {
+        this.$emit("delete", this.index);
+      }
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
